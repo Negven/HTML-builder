a44ad1fafc4c267e012207799bfafa68eca24a4a
Yevhenii Shevchenko <supershevchenko548@gmail.com>
2022-05-21 16:29:42 +0200

    fix: add remove dir

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,13 +3,14 @@ const path = require('path');
 
 (async () => {
   let dir = await fs.promises.readdir(path.join(__dirname, 'files'), {withFileTypes: true}, err => console.log(err));
-  await fs.mkdir(path.join(__dirname, 'files-copy'), err => err);
+  await fs.promises.rm(path.join(__dirname, 'files-copy'),{ force: true, recursive: true }, err => err);
+  await fs.promises.mkdir(path.join(__dirname, 'files-copy'), { force: true }, err => err);
   for(let fileIndex in dir) {
     let file = dir[fileIndex];
     if(file.isFile()) {
       const oldLink = path.join(__dirname, 'files', file.name);
       const newLink = path.join(__dirname, 'files-copy', file.name);
-      await fs.copyFile(oldLink, newLink, err => err);
+      fs.copyFile(oldLink, newLink, err => err);
     }
   }
 })();
