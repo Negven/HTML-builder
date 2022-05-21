ac6b9e4b164e6cbf2a047a806280a419bd8d3502
Yevhenii Shevchenko <supershevchenko548@gmail.com>
2022-05-21 02:35:59 +0200

    feat: do 4-th task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,15 @@
+const fs = require('fs');
+const path = require('path');
 
+(async () => {
+  let dir = await fs.promises.readdir(path.join(__dirname, 'files'), {withFileTypes: true}, err => console.log(err));
+  await fs.mkdir(path.join(__dirname, 'files-copy'), err => err);
+  for(let fileIndex in dir) {
+    let file = dir[fileIndex];
+    if(file.isFile()) {
+      const oldLink = path.join(__dirname, 'files', file.name);
+      const newLink = path.join(__dirname, 'files-copy', file.name);
+      await fs.copyFile(oldLink, newLink, err => err);
+    }
+  }
+})();
