40058c860d134dfb48a14bfdd7c3bcd443858625
Yevhenii Shevchenko <supershevchenko548@gmail.com>
2022-05-21 12:25:18 +0200

    refactor: dealet console log in 5-th task

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -9,7 +9,6 @@ const {createWriteStream, createReadStream} = require('fs');
   for(let fileIndex in dir) {
     let file = dir[fileIndex];
     if(file.isFile() && path.parse(path.join(__dirname, file.name)).ext === '.css') {
-      console.log(file);
       const readStream = createReadStream(path.join(__dirname, 'styles', file.name));
       readStream.pipe(writeStream);
     }
